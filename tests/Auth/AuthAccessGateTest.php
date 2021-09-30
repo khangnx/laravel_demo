49c38e368050bec9f5ad2a9f929d2b4d821cabdb
khangnx <xuankhangitbt@gmail.com>
2021-09-30 09:43:11 +0700

    init

--- tests/Auth/AuthAccessGateTest.php
+++ tests/Auth/AuthAccessGateTest.php
@@ -31,7 +31,7 @@ class AuthAccessGateTest extends TestCase
     public function testBeforeCanTakeAnArrayCallbackAsObject()
     {
         $gate = new Gate(new Container, function () {
-            //aaaaaa
+            //aaaaaadddd
         });
 
         $gate->before([new AccessGateTestBeforeCallback, 'allowEverything']);
